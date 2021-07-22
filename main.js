21f77e30b5e046ddadd473dbd93b148a71ea8155
emilianoberon10 <emilianoberon10@gmail.com>
2021-07-22 17:07:37 -0300

    documents

---
+++ main.js
@@ -0,0 +1,18 @@
+window.onload =()=>{
+    h=0; m=0; s=0; ms=0; timeStarted=0;
+    clock = document.getElementById('clock')
+    btnStar = document.getElementById('btnstart')
+    btnStop = document.getElementById('btnstop')
+    btnReset = document.getElementById('btnreset')
+    
+}
+
+
+function starClock(e){
+    e.preventDefault();
+
+}
+function stopClock(e)
+btnStar.addEventListener('click', starClock);
+btnStop.addEventListener('click');
+btnReset.addEventListener('click');
